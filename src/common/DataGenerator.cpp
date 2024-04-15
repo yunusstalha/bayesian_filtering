186a72be66bb9e434f770ceba28cefca2f561282
yunusstalha <yunus.terzurumlu@gmail.com>
2024-04-15 12:53:39 +0300

    Some License stuff

--- src/common/DataGenerator.cpp
+++ src/common/DataGenerator.cpp
@@ -1,3 +1,24 @@
+/***************************************************************************
+ * BayesianFiltering - A Modern C++ Bayesian Filtering Library
+ *
+ * Copyright (C) 2024 yunusstalha
+ * For more information see https://github.com/yunusstalha/bayesian_filtering
+ *
+ * BayesianFiltering is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License as published by the
+ * Massachusetts Institute of Technology.
+ *
+ * BayesianFiltering is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MIT License for more details.
+ *
+ * You should have received a copy of the MIT License
+ * along with BayesianFiltering.  If not, see <https://opensource.org/licenses/MIT>.
+ *
+ * Author: yunsstalha 
+ ***************************************************************************/
+
 #include "common/DataGenerator.hpp"
 #include <iostream>
 
